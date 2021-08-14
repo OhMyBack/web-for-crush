f676543e1208594163147cbff1b279214e4ccc6b
OhMyBack <88929778+OhMyBack@users.noreply.github.com>
2021-08-14 19:22:32 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
+  text1: "He luu Phưn!",
+  text2: "Nam có điều này muốn hỏi Phưn, nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text5: "Nam mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text7: "lí do Phưn thích Nam đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì Nam đẹp try vlllll, deep try siêu cấp vũ trụ",
+  text10: "Nam biết mà ^^ Yêu Phưn 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Sau dịch Nam qua đón Phưn đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko nói lời yêu thưn đi nàooo",
   text12: "Okii lunn <3",
 };
 
